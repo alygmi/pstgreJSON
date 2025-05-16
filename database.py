77f4d3482ba03716b35bb978410b1cb744ab8b57
alygmi <nurrusulialadiyat@gmail.com>
2025-05-16 16:58:03 +0700

    database.py modification

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
@@ -12,6 +12,7 @@ engine = create_engine(DATABASE_URL)
 Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = Sessionlocal()
